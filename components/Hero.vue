66d09a9b8d69bc961d11d7312c5c1cfcf775cc5e
Kareem basuony <kareembasuonyg@gmail.com>
2021-12-06 01:42:47 +0200

    Adding hero section

---
+++ components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-img
+    :src="
+      require('~/assets/young-attractive-maid-uniform-showing-ok-gesture-while-standing.jpeg')
+    "
+    max-height="100vh"
+    class="align-center"
+    gradient="to right, #D7AF4D 30%, transparent 50%"
+    position="right top"
+  >
+    <v-row class="text-center">
+      <v-col cols="12" md="4">
+        <v-img
+          class="mx-auto"
+          :src="require('~/assets/grey-logo.png')"
+          max-width="200"
+          max-height="100"
+        ></v-img>
+        <p class="secondary--text text-h5">مكتب اختيار القمة للخادمات</p>
+        <v-btn outlined color="secondary">اتصل بنا</v-btn>
+      </v-col>
+    </v-row>
+  </v-img>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,29 +9,37 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - top-pick',
-    title: 'top-pick',
+    titleTemplate: 'مكتب اختيار القمة للخادمات',
+    title: 'مكتب اختيار القمة للخادمات',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/png', href: '/logo.png' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true,
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=El+Messiri&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['~/assets/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -45,29 +53,27 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: '#D7AF4D',
           accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          secondary: '#dbd9da',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
